26f679de518c4b03dea9df4683028429e7641249
Bijen Hirachan <bijen.hirachan@gmail.com>
2021-06-30 22:28:08 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>My reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Results :points = "points" v-if="points"/>
+  <Block v-if="isPlaying" :delay="delay" @reaction-time="hideBox"/>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'App',
+  components: { 
+    Block,
+    Results
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      points: null
+    }
+  },
+  methods:{
+    start(){
+      this.points = null;
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      
+    },
+    hideBox(result){
+      this.isPlaying = false;
+      this.points = result;
+      console.log(result);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>Your tier : {{ tier }}</h3>
+        <h5>Your speed : {{ points }} ms</h5>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['points'],
+    data(){
+        return{
+            tier: null,
+        }
+    },
+    mounted(){
+        if(this.points < 300){
+            this.tier = "Ace";
+        }else if(this.points < 700){
+            this.tier = "Diamond";
+        }else if(this.points < 1000){
+            this.tier = "Gold";
+        }else{
+            this.tier = "Bronze";
+        }
+    }
+}
+</script>
